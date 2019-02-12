a3119c85e6e581b9c6da472b4f3b4262baebbf6c
Izabela <izabela.nowosada@gmail.com>
2019-02-12 11:23:16 +0100

    add pages

---
+++ src/pages/controller.ts
@@ -0,0 +1,17 @@
+import { JsonController, Get, Param } from 'routing-controllers'
+import pagesById, { Page } from './data'
+// this makes sure a class is marked as controller that always returns JSON
+// perfect for our REST API
+@JsonController()
+export default class PageController {
+// this markes a method as endpoint
+// in this case it responds to any GET /pages/:id
+// request with :id being a variable parameter
+    @Get('/pages/:id')
+    getPage(
+      // this decorator retrieves the ID parameter from the url
+        @Param('id') id: number
+    ): Page {
+        return pagesById[id]
+    }
+}
