63fb2f3aede1de44eb2b309705bb14741ec58040
Izabela <izabela.nowosada@gmail.com>
2019-02-12 16:31:44 +0100

    config heroku

--- src/index.ts
+++ src/index.ts
@@ -30,6 +30,6 @@ const app = createKoaServer({
 
 setupDb()
   .then(_ =>
-    app.listen(4000, () => console.log('Listening on port 4000'))
+    app.listen(process.env.PORT, 4000, () => console.log('Listening on port 4000'))
   )
   .catch(err => console.error(err))
